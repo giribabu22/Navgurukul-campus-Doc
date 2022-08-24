be6285f30cb25805a136d2c5b47c0f8feebe752e
giribabu <102803078+giribabu22@users.noreply.github.com>
2022-08-24 09:40:50 +0530

    Update activity_timeTable.py

---
+++ activity_timeTable.py
@@ -0,0 +1,37 @@
+Navgurukul campus
+August to November activity time-table  
+
+    • August
+    1. 18/August/2022 	-   Movie-in-English
+    2. 19/August/2022   -   Progress tracking
+    3. 22/August/2022   -   Mock-test
+    4. 29/August/2022 	-   Progress tracking
+
+    • September
+    1. 01/September/2022    -   Hackathon 
+    2. 05/September/2022    -   Mock-test
+    3. 10/September/2022   -    Progress tracking
+    4. 19/September/2022   -    Mock-test
+    5. 20/September/2022   -    Progress tracking
+    6. 25/September/2022   -    Movie-in-English
+    7. 30/September/2022   -    Progress tracking
+
+    • October
+    1. 01/October/2022        -   Hackathon 
+    2. 03/October/2022         -  Mock-test
+    3. 10/October/2022        -   Progress tracking
+    4. 17/October/2022         -  Mock-test
+    5. 20/October/2022        -   Progress tracking
+    6. 25/October/2022        -   Movie-in-English
+    7. 29/October/2022        -   Progress tracking
+    8. 31/October/2022        -   Mock-test
+
+    • November
+    1. 01/November/2022    -    Hackathon
+    2. 10/November/2022    -    Progress tracking
+    3. 11/November/2022     -   Mock-test
+    4. 12/November/2022 	-   Movie-in-English
+
+
+
+   Happy learning guys
