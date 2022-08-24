d35c3ef63fe4365a6844946a5f52ed7a8f58408d
giribabu <102803078+giribabu22@users.noreply.github.com>
2022-08-24 09:43:43 +0530

    Update Training&PlacementCoordinatorRules.py

--- Training&PlacementCoordinatorRules.py
+++ Training&PlacementCoordinatorRules.py
@@ -1,7 +1,7 @@
 As a Training & Placement coordinator rules
-    1) The class will start on 8:40 am every day.
+    1) The class will start on 8:45 am.
     2) Every 10 days once progress tracking.
-    3) Every month 1 or 2 dates will be a hackathon.
+    3) Every month once Hackathon.
     4) Every 10 to 15 days once mock-test.
     5) During class time, Everyone should be in a classroom not in the room or outside of the campus.
     6) If any student is in the red zone that student needs to follow the mentor rules strictly.
